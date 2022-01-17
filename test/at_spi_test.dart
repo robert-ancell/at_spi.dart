1e20a957141ec0bb94f1f1d998085c92adbb3e53
Robert Ancell <robert.ancell@canonical.com>
2022-01-17 16:14:59 +1300

    Fix test

--- test/at_spi_test.dart
+++ test/at_spi_test.dart
@@ -1,9 +1,10 @@
-import 'dart:io';
-
-import 'package:dbus/dbus.dart';
 import 'package:at_spi/at_spi.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('version', () async {});
+  test('client', () async {
+    var client = AtSpiClient();
+    await client.connect();
+    await client.close();
+  });
 }
